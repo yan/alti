824d7c3cc99036b421eefefc967e84e9f8542eb4
yan <rottled@gmail.com>
2015-06-22 15:55:41 -0400

    Began bmx055 support

---
+++ firmware/include/bmx055.h
@@ -0,0 +1,8 @@
+/**
+ *
+ *
+ */
+
+void bmx055_reset(void);
+
+void bmx055_init(void);
---
+++ firmware/src/bmx055.c
@@ -0,0 +1,20 @@
+/**
+ *
+ *
+ */
+#include <bmx055.h>
+
+#define BYTEORDER BYTEORDER_MSB
+#define USE_SPI
+
+#include <transfer_macros.h>
+
+
+void bmx055_reset(void)
+{
+
+}
+
+void bmx055_init(void)
+{
+}
